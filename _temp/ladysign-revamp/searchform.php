fa833c85fdb7d8dedeb7c949fb5a1dc87b6ec691
Lee Boonstra <me@leeboonstra.com>
2018-11-23 18:21:10 +0100

    finetune last styles and enable service worker

--- _temp/ladysign-revamp/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-	</label>
-	<button type="submit" class="btn btn-primary search-submit"><?php echo esc_attr_x( 'Search', 'submit button' ) ?></button>
-</form>
