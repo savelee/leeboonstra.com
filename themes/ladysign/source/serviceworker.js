d3117efec4469a482b39e51dbdb0616885d77d7a
Lee Boonstra <me@leeboonstra.com>
2018-11-23 19:29:44 +0100

    fix sw

--- themes/ladysign/source/serviceworker.js
+++ themes/ladysign/source/serviceworker.js
@@ -1,95 +1,44 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('cache-pf.js');
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const VERSION = '2';
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  '/index.html',
-  './', // Alias for index.html
-  '/assets/styles/main.css',
-  '/assets/js/main.js',
-  '/assets/js/vendor/jquery-3.3.1.min.js',
-  '/assets/js/vendor/modernizr-3.6.0.min.js',
-  "/assets/fonts/Mathlete-Skinny-webfont.eot",
-  "/assets/fonts/Mathlete-Skinny-webfont.svg",
-  "/assets/fonts/Mathlete-Skinny-webfont.ttf",
-  "/assets/fonts/Mathlete-Skinny-webfont.woff",
-  "/assets/fonts/oswald-regular-webfont.eot",
-  "/assets/fonts/oswald-regular-webfont.svg",
-  "/assets/fonts/oswald-regular-webfont.ttf",
-  "/assets/fonts/oswald-regular-webfont.woff",
-  "/assets/fonts/Roboto-Regular-webfont.eot",
-  "/assets/fonts/Roboto-Regular-webfont.svg",
-  "/assets/fonts/Roboto-Regular-webfont.ttf",
-  "/assets/fonts/Roboto-Regular-webfont.woff",
-  "/assets/fonts/icomoon.eot",
-  "/assets/fonts/icomoon.svg",
-  "/assets/fonts/icomoon.ttf",
-  "/assets/fonts/icomoon.woff",
-  "/images/book.gif",
-  "/images/profile.jpg"
-];
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+        'assets/styles/main.css',
+        'assets/fonts/icomoon.eot',
+        'assets/fonts/icomoon.svg',
+        'assets/fonts/icomoon.ttf',
+        'assets/fonts/icomoon.woff'
+      ]);
+    })
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
+    })
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
 });
