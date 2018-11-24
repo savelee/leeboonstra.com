3482945f24df42909d9371641a472a3f60e2089f
Lee Boonstra <me@leeboonstra.com>
2018-11-24 17:52:18 +0100

    sw

--- themes/ladysign/source/serviceworker.js
+++ themes/ladysign/source/serviceworker.js
@@ -3,60 +3,46 @@
 importScripts('cache-pf.js');
 
 var CACHE_NAME = "devtricks";
-
-// Here comes the install event!
-// This only happens once, when the browser sees this
-// version of the ServiceWorker for the first time.
-self.addEventListener('install', function(event) {
-  // We pass a promise to event.waitUntil to signal how 
-  // long install takes, and if it failed
-  event.waitUntil(
-    // We open a cache…
-    caches.open('simple-sw-v1').then(function(cache) {
-      // And add resources to it
-      return cache.addAll([
-        './',
-        '/index.html',
-        '/assets/styles/main.css',
-        '/assets/js/vendor/jquery-3.3.1.min.js',
-        '/assets/js/vendor/modernizr-3.6.0.min.js',
-        '/assets/js/vendor/popper.min.js',
-        '/assets/js/vendor/bootstrap.min.js', 
-        '/assets/js/disqus-lazy-min.js',      
-        '/cache-pf.js',
-        '/favicon.ico',
-        '/manifest.json',
-        '/images/profile.jpg',
-        '/images/profile_small.jpg',        
-        '/images/book.gif',
-        '/images/leeboonstra-speaker-500x351.png',
-        '/images/10yearsold-500x326.png',
-        '/images/lee.boonstra-resume.pdf',
-        '/images/leeboonstra-book.png',
-        '/assets/fonts/icomoon.eot',
-        '/assets/fonts/icomoon.svg',
-        '/assets/fonts/icomoon.ttf',
-        '/assets/fonts/icomoon.woff',
-        '/assets/fonts/consolas-webfont.eot',
-        '/assets/fonts/consolas-webfont.svg',
-        '/assets/fonts/consolas-webfont.ttf',
-        '/assets/fonts/consolas-webfont.woff',
-        "/assets/fonts/Mathlete-Skinny-webfont.eot",
-        "/assets/fonts/Mathlete-Skinny-webfont.svg",
-        "/assets/fonts/Mathlete-Skinny-webfont.ttf",
-        "/assets/fonts/Mathlete-Skinny-webfont.woff",
-        "/assets/fonts/oswald-regular-webfont.eot",
-        "/assets/fonts/oswald-regular-webfont.svg",
-        "/assets/fonts/oswald-regular-webfont.ttf",
-        "/assets/fonts/oswald-regular-webfont.woff",
-        "/assets/fonts/Roboto-Regular-webfont.eot",
-        "/assets/fonts/Roboto-Regular-webfont.svg",
-        "/assets/fonts/Roboto-Regular-webfont.ttf",
-        "/assets/fonts/Roboto-Regular-webfont.woff"
-      ]);
-    })
-  );
-});
+var REQUIRED_FILES = [
+  './',
+  '/index.html',
+  '/assets/styles/main.css',
+  '/assets/js/vendor/jquery-3.3.1.min.js',
+  '/assets/js/vendor/modernizr-3.6.0.min.js',
+  '/assets/js/vendor/popper.min.js',
+  '/assets/js/vendor/bootstrap.min.js', 
+  '/assets/js/disqus-lazy-min.js',      
+  '/cache-pf.js',
+  '/favicon.ico',
+  '/manifest.json',
+  '/images/profile.jpg',
+  '/images/profile_small.jpg',        
+  '/images/book.gif',
+  '/images/leeboonstra-speaker-500x351.png',
+  '/images/10yearsold-500x326.png',
+  '/images/lee.boonstra-resume.pdf',
+  '/images/leeboonstra-book.png',
+  '/assets/fonts/icomoon.eot',
+  '/assets/fonts/icomoon.svg',
+  '/assets/fonts/icomoon.ttf',
+  '/assets/fonts/icomoon.woff',
+  '/assets/fonts/consolas-webfont.eot',
+  '/assets/fonts/consolas-webfont.svg',
+  '/assets/fonts/consolas-webfont.ttf',
+  '/assets/fonts/consolas-webfont.woff',
+  "/assets/fonts/Mathlete-Skinny-webfont.eot",
+  "/assets/fonts/Mathlete-Skinny-webfont.svg",
+  "/assets/fonts/Mathlete-Skinny-webfont.ttf",
+  "/assets/fonts/Mathlete-Skinny-webfont.woff",
+  "/assets/fonts/oswald-regular-webfont.eot",
+  "/assets/fonts/oswald-regular-webfont.svg",
+  "/assets/fonts/oswald-regular-webfont.ttf",
+  "/assets/fonts/oswald-regular-webfont.woff",
+  "/assets/fonts/Roboto-Regular-webfont.eot",
+  "/assets/fonts/Roboto-Regular-webfont.svg",
+  "/assets/fonts/Roboto-Regular-webfont.ttf",
+  "/assets/fonts/Roboto-Regular-webfont.woff"
+];
 
 //The install event on the service worker will
 //open the cache and use addAll to direct
@@ -71,6 +57,7 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
 //the fetch event of a service worker is fired for
 //every single request the page makes. The fetch
 //event also allows you to serve alternate content
