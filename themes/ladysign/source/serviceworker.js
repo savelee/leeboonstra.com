439e7f99d865530c19c7c43269dc45282c4d4472
Lee Boonstra <me@leeboonstra.com>
2018-11-24 17:15:00 +0100

    on network service worker

--- themes/ladysign/source/serviceworker.js
+++ themes/ladysign/source/serviceworker.js
@@ -56,21 +56,15 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// The fetch event happens for the page request with the
-// ServiceWorker's scope, and any request made within that
-// page
 self.addEventListener('fetch', function(event) {
-  // Calling event.respondWith means we're in charge
-  // of providing the response. We pass in a promise
-  // that resolves with a response object
   event.respondWith(
-    // First we look for something in the caches that
-    // matches the request
-    caches.match(event.request).then(function(response) {
-      // If we get something, we return it, otherwise
-      // it's null, and we'll pass the request to
-      // fetch, which will use the network.
-      return response || fetch(event.request);
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
